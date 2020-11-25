225c0023bc365cb26132871c5fe42272b582acf6
Christian Banal <christian.banal@trustvesta.com>
2020-11-25 14:19:47 -0800

    first commit

---
+++ passportConfig.js
@@ -0,0 +1,77 @@
+const sql = require("mssql");
+const bcrypt = require("bcrypt");
+const localStrategy = require("passport-local").Strategy;
+
+const config = {
+  user: "sa",
+  password: "Password1",
+  server: "localhost\\S2012", // You can use 'localhost\\instance' to connect to named instance
+  database: "testDatabase",
+  options: { encrypt: false },
+};
+
+module.exports = function (passport) {
+  passport.use(
+    new localStrategy(async (username, password, done) => {
+      let user = await getUser(username);
+      if (user instanceof Error) {
+        throw user;
+      } else if (user) {
+        await bcrypt.compare(password, user.password, (err, result) => {
+          if (err) throw err;
+          if (result === true) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        });
+      }
+    })
+  );
+  passport.serializeUser((user, cb) => {
+    cb(null, user.Id);
+  });
+
+  passport.deserializeUser(async (id, cb) => {
+    let user = await getUserByID(id);
+    if (user instanceof Error) {
+      throw user;
+    } else if (user) {
+      cb(null, user);
+    }
+  });
+};
+getUser = async (username) => {
+  try {
+    let pool = await sql.connect(config);
+    let result1;
+    await pool
+      .request()
+      .input("username", sql.NVarChar, username)
+      .query("select * from account where username = @username")
+      .then((res) => (result1 = res))
+      .then(() => pool.close());
+    return result1.recordset[0];
+  } catch (err) {
+    // ... error checks
+    console.log(err);
+    throw err;
+  }
+};
+getUserByID = async (id) => {
+  try {
+    let pool = await sql.connect(config);
+    let result1;
+    await pool
+      .request()
+      .input("id", sql.Int, id)
+      .query("select * from account where Id = @id")
+      .then((res) => (result1 = res))
+      .then(() => pool.close());
+    return result1.recordset[0];
+  } catch (err) {
+    // ... error checks
+    console.log(err);
+    throw err;
+  }
+};
---
+++ server.js
@@ -0,0 +1,98 @@
+//const mongoose = require("mongoose");
+const sql = require("mssql");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const User = require("./user");
+let apiPort = 4000;
+
+const app = express();
+
+const config = {
+  user: "sa",
+  password: "Password1",
+  server: "localhost\\S2012", // You can use 'localhost\\instance' to connect to named instance
+  database: "testDatabase",
+  options: { encrypt: false },
+};
+// const db_name = "test";
+// const db_user = "devuser";
+// const db_pass = "devuser";
+
+// mongoose.connect(
+//   `mongodb+srv://${db_user}:<password>@cluster0.uoidc.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   () => {
+//     console.log(`Mongoose connected to ${db_name} as ${db_user}`);
+//   }
+// );
+
+//Middleware -------------------------------------------------------------
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
+);
+app.use(session({ secret: "sectr", resave: true, saveUninitialized: true }));
+app.use(cookieParser("sectr"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig")(passport);
+
+//Routes -------------------------------------------------------------
+app.post("/login", async (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log("Authernticated User(stored to session) : " + req.user);
+      });
+    }
+  })(req, res, next);
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    let pool = await sql.connect(config);
+    let result1;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await pool
+      .request()
+      .input("username", sql.NVarChar, req.body.username)
+      .input("password", sql.NVarChar, hashedPassword)
+      .query(
+        "INSERT INTO [dbo].[account] ([username],[password]) VALUES (@username, @password); SELECT SCOPE_IDENTITY() AS id;"
+      )
+      .then((res) => (result1 = res.recordset[0].id))
+      .then(() => pool.close());
+
+    res.send("account created at ID : " + result1);
+  } catch (err) {
+    // ... error checks
+    console.log(err);
+    res
+      .status(400)
+      .send(new Error("Failed to register account due to : " + err));
+  }
+});
+
+app.get("/getUser", async (req, res) => {});
+
+app.listen(apiPort, () => {
+  console.log("Auth Server started at port : " + apiPort);
+});
