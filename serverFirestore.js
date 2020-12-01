07893d31824b1d1e4458efa5a08751ba8efc045e
Christian Banal <christian.banal@trustvesta.com>
2020-12-01 15:51:54 -0800

    logging

--- serverFirestore.js
+++ serverFirestore.js
@@ -12,7 +12,11 @@ const bodyParser = require("body-parser");
 let apiPort = 4000;
 
 const app = express();
-
+console.log(
+  JSON.parse(
+    Buffer.from(process.env.GOOGLE_CONFIG_BASE64, "base64").toString("ascii")
+  )
+);
 //initialize Firestore
 admin.initializeApp({
   credential: firebaseAdminSdk.credential.cert(
